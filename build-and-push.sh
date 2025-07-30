55eb60bc04e954cbfff0b44740fd671215dc04cc
Juan Caballol Avendano <jcaballo@redhat.com>
2025-07-30 14:30:19 -0400

    update

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,3 +1,3 @@
 mvn clean package -Dquarkus.package.type=uber-jar
-podman build --platform linux/amd64 -t quay.io/mcaballol/camel-rest-status .
-podman push quay.io/mcaballol/camel-rest-status
+podman build --platform linux/amd64 -t quay.io/mcaballol/camel-rest-status:v2 .
+podman push quay.io/mcaballol/camel-rest-status:v2
