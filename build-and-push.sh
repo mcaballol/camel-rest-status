0da572e4c7451e53d4a400031225f1b7c66006a8
Juan Caballol Avendano <jcaballo@redhat.com>
2025-07-30 03:57:54 -0400

    add applicationset

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,3 +1,3 @@
-mvn clean package
+mvn clean package -Dquarkus.package.type=uber-jar
 podman build --platform linux/amd64 -t quay.io/mcaballol/camel-rest-status .
 podman push quay.io/mcaballol/camel-rest-status
